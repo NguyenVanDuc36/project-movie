09dc040c7f0325a63ab2fd312b929fe360a2878b
NguyenVanDuc36 <duc.nv3134@sinhvien.hoasen.edu.vn>
2021-09-29 06:55:50 +0700

    master2

--- src/templates/HomeTemplate/layout/DetailHometemplate.js
+++ src/templates/HomeTemplate/layout/DetailHometemplate.js
@@ -4,7 +4,7 @@ import { Route } from 'react-router';
 // import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import Header from "./Header/Header";
-import HeaderDetail from './Header/HeaderDetail';
+// import HeaderDetail from './Header/HeaderDetail';
 
 export const DetailTempalte = (props) => {
     const {Component,...restProps} = props;
